e737f679ba67d2b3c7a75c5f1f6f261a2221b351
OCTAVIO LEO SCHWARZBERG <47297655@ort>
2023-07-07 10:08:02 -0300

    s

--- src/Controllers/picadurasController.js
+++ src/Controllers/picadurasController.js
@@ -1,4 +1,4 @@
-import { Router} from 'express';
+import { Router } from 'express';
 import PicadurasServices from '../Services/Picaduras-services';
 
 const router = Router();
@@ -6,26 +6,45 @@ const picaduraService = new PicadurasServices();
 
 
 
-router.get('/:id', async (req, res) =>{
-console.log("Estoy en picadura controller get by id");
-let respuesta;
+router.get('/:id', async (req, res) => {
+    console.log("Estoy en picadura controller get by id");
+    let respuesta;
 
-const picadura = await pizzaService.getPicaduraById(req.params.id);
-if(picadura!=null) {
+    const picadura = await picaduraService.getPicaduraById(req.params.id);
+    if (picadura != null) {
 
-    respuesta = res.status(200).json(picadura);
-}
-else{
-    respuesta = res.status(404).send("no se encontro la picadura"); 
-}
-return respuesta;
+        respuesta = res.status(200).json(picadura);
+    }
+    else {
+        respuesta = res.status(404).send("no se encontro la picadura");
+    }
+    return respuesta;
 
 });
 
 
 
-router.post('', async (req, res) =>{
-console.log('Estoy en picadura controller insert');
+router.post('', async (req, res) => {
+    console.log('Estoy en picadura controller insert');
 
-const picadura = await picaduraService.
+    const picadura = await picaduraService.insertPicadura(req.body);
+
+    return res.status(201).json(picadura);
 });
+
+router.put('/:id', async (req, res) => {
+    console.log('Estoy en picadura controller update');
+
+    const picadura = await picaduraService.updatePicadura(req.body);
+
+    return res.status(201).json(picadura);
+});
+
+router.delete('/:id', async (req, res) => {
+    console.log('Estoy en picadura controller delete');
+
+    const picadura = await picaduraService.deletePicaduraById(req.id);
+
+    return res.status(201).json(picadura);
+});
+

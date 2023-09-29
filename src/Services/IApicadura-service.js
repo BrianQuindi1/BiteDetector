e72299ed94287b2e11f2c72d7503ba9e3043fd1c
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-09-29 10:44:29 -0300

    AS

--- src/Services/IApicadura-service.js
+++ src/Services/IApicadura-service.js
@@ -58,7 +58,7 @@ export default class IApicaduraService {
         console.log(`top5 ${predictions}`);
         //predictions.sort((a,b)=> b-a); //capaz hay que agregar un predictions= 
         console.log(predictions);
-        console.log(top5[0]);
+        console.log("tp5 ", top5[0]);
         let estado=this.calcularEstado(top5[0].probability);
         console.log("ESTADO" + estado);
         let IdInsecto= await insectoService.getByName(top5[0].className)

fdefae3ebd54ffb5f820619036a5d4d05026e9fc
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-09-29 11:09:58 -0300

    AS

--- src/Services/Insecto-services.js
+++ src/Services/Insecto-services.js
@@ -13,7 +13,7 @@ export default class InsectoService{
             let pool = await sql.connect(config);
             let result = await pool.request()
                                     .input('pNombre', sql.VarChar, nombre)
-                                    .query('SELECT IdInsecto FROM Insecto WHERE Nombre = @pNombre');
+                                    .query(`SELECT IdInsecto FROM Insecto WHERE Nombre = '@pNombre'`);
             returnEntity = result.recordsets[0]; //[0]
             console.log(returnEntity);
         } catch (error) {

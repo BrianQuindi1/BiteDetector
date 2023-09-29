fbeb1d18320db0bc754209f6d67d592b5bfffde0
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-09-29 11:01:52 -0300

    AS

--- src/Services/Insecto-services.js
+++ src/Services/Insecto-services.js
@@ -5,14 +5,16 @@ import config from '../../dbconfig.js'
 
 export default class InsectoService{
     getByName = async (nombre) => {
+        
         let returnEntity = null;
         console.log('Estoy en: InsectoService.GetByName(nombre)');
+        console.log("el nombre que me esta llegandoe es", nombre);
         try {
             let pool = await sql.connect(config);
             let result = await pool.request()
                                     .input('pNombre', sql.VarChar, nombre)
                                     .query('SELECT IdInsecto FROM Insecto WHERE Nombre = @pNombre');
-            returnEntity = result.recordsets[0][0]; //[0]
+            returnEntity = result.recordsets[0]; //[0]
             console.log(returnEntity);
         } catch (error) {
             console.log(error);

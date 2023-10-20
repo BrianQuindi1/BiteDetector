d118c0f5ac62378df39021047824305b6d850951
OCTAVIO LEO SCHWARZBERG <47297655@ort>
2023-10-20 10:41:29 -0300

    devuelve diagnostico

--- src/Controllers/insectoController.js
+++ src/Controllers/insectoController.js
@@ -4,9 +4,20 @@ import InsectoService from '../Services/Insecto-services.js';
 
 const router = Router();
 
-const InsectoSrc = new InsectoService();
+const insectoSrc = new InsectoService();
 router.get('/:id', async (req, res) => {
-    
+    console.log("Estoy en picadura controller get by id");
+    let respuesta;
+
+    const insecto = insectoSrc.getInsecto(req.params.id);
+    if (insecto != null) {
+
+        respuesta = res.status(200).json(insecto);
+    }
+    else {
+        respuesta = res.status(404).send("no se encontro el insecto");
+    }
+    return respuesta;
 });
 
 export default router;
